d53db3726f8c4ef3a6173fadb50a21eb3a9e9aeb
docgreenrob <doc.green.rob@gmail.com>
2018-03-09 11:25:53 -0800

    Adding WCF Service and Client

---
+++ WCFClient/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WCFClient.HelloUniverseServiceReference;
+
+namespace WCFClient
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			HumanClient client = new HumanClient("NetTcpBinding_IHuman");
+			Human human = new Human
+			{
+				Race = "African",
+				BirthDate = new DateTime(1979, 8, 28),
+				Gender = "Male",
+				FirstName = "Robert",
+				LastName = "Green"
+			};
+			Console.WriteLine(client.Speak(human));
+			Console.ReadKey();
+		}
+	}
+}
